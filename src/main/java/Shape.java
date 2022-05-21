a33cba9c682648878d6be7777729ba78ea629530
JoannaMorgas <asia.morgas@gmail.com>
2022-05-21 18:49:15 +0200

    refactor returnMessage()

--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -2,16 +2,16 @@ public sealed interface Shape permits Circle, Polygon {
     abstract String getArea();
 
     default String getDiameter() {
-        return switch (this) {
-            case this instanceof Circle -> this.returnMessage();
-            case this instanceof Section -> this.returnMessage();
-            case this instanceof Triangle -> this.returnMessage();
-            default -> String.format("getDiameter() called for instance of %s", this);
+        return switch(this) {
+            case this instanceof Circle -> this.returnMessage("Circle");
+            case this instanceof Section -> this.returnMessage("Section");
+            case this instanceof Triangle -> this.returnMessage("Triangle");
+            default -> this.returnMessage("Object");
         };
     }
 
-    private String returnMessage() {
-        return String.format("getDiameter() called for instance of %s", this.getClass());
+    private String returnMessage(String str) {
+        return String.format("getDiameter() called for instance of ", str);
     }
 
 }
